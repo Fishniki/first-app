1285464747a3c1b459f37376af3a1ddb3056fb7b
Fishniki <nikichum96@gmail.com>
2024-07-10 20:44:15 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-red-500">Hello Word</h1>
+    </div>
+  );
+}
