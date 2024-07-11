3b4cbfdbb57d973e00d2a9c4819200fe70fc4e1e
Fishniki <nikichum96@gmail.com>
2024-07-11 21:24:30 +0700

    9:15

--- src/pages/shop/[[...slug]].tsx
+++ src/pages/shop/[[...slug]].tsx
@@ -10,6 +10,7 @@ const ShopPage = () => {
             <div>Shop Page</div>
             <p>Shop : {`${query.slug && query.slug[0]}`}</p>
             <p>Keterangan: {`${query.slug ? query.slug[0]: "Data Kosong"}`}</p>
+            <p>Keterangan2: {query.slug ? query.slug.join(', ') : "Data Kosong"}</p>
             <p className="">
                 shop 1: {`${query.slug && query.slug[0]}`}
                 shop 2: {`${query.slug && query.slug[1]}`}
